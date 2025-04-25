471107c800226360616527195b6b7112fb80e677
Victoria Banjo <67071771+banjovic@users.noreply.github.com>
2025-04-25 10:20:21 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner_generator/generate_qr_code.dart';
+import 'package:qr_code_scanner_generator/scan_qr_code.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'QR Code Scanner and Generator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QR Code Scanner and Generator'), backgroundColor: Colors.lightBlue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: (){
+              setState(() {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>ScanQrCode()));
+              });
+            }, child: Text('Scan QR Code')),
+            SizedBox(height: 40,),
+            ElevatedButton(onPressed: (){
+              setState(() {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>GenerateQrCode()));
+              });
+            }, child: Text('Generate QR Code'))
+          ],
+        ),
+      ),
+    );
+  }
+}
